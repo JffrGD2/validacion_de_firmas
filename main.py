330b7b56f40d336dc04dfb9a96534c252961d70b
Oscar-Noe-LP <debriuwu77@gmail.com>
2025-04-04 10:42:57 -0600

    api v0.2

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import fastapi 
+from fastapi import FastAPI, HTTPException
+import logging
+from fastapi.middleware.gzip import GZipMiddleware
 import sqlite3
 
-conn = sqlite3.connect('/content/BDFirmas.db')
-cursor = conn.cursor()
-print("Conexión exitosa a la base de datos")
- 
- 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tablas = cursor.fetchall()
-print("tablas =", [tabla[0] for tabla in tablas])
- 
- 
-conn.close()
+# Configuración de logs
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "Bienvenido a la API de BDFirmas"}
+
+def conectar():
+    return sqlite3.connect("BDFirmas.db")
+
+@app.get("/tablas")
+def tablas():
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tablas = cursor.fetchall()
+    conexion.close()
+    if not tablas:
+        raise HTTPException(status_code=404, detail="No hay tablas en la base de datos")
+    else:
+        return [{"tablas": tabla[0]} for tabla in tablas]
